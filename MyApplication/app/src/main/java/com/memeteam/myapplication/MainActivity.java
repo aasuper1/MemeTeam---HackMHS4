dcc258f154b57c3adda01d98c0652add7ebadfee
Curse-Ka <curse.ka@gmail.com>
2017-05-27 23:16:59 -0400

    Lots of work and stuff

--- MyApplication/app/src/main/java/com/memeteam/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/memeteam/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.memeteam.myapplication;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +14,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     BluetoothAdapter btAdapter;
     Set<BluetoothDevice> devicesArray;
+    ArrayList<String> pairedDevices;
+    IntentFilter filter;
+    BroadcastReceiver reciever;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +48,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getPairedDevices() {
-        ddevicesArray - btAdapter.getBondedDevices();
-        if(devicesArray.size()>0)
+        devicesArray = btAdapter.getBondedDevices();
+        if(devicesArray.size()>0){
+            for(BluetoothDevice device:devicesArray){
+                pairedDevices.add(device.getName());
+            }
+        }
     }
 
     private void init() {
@@ -51,6 +62,21 @@ public class MainActivity extends AppCompatActivity {
         listAdapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,0);
         listView.setAdapter(listAdapter);
         btAdapter = BluetoothAdapter.getDefaultAdapter();
+        pairedDevices = new ArrayList<String>();
+        filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        reciever = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+
+                if (BluetoothDevice.ACTION_FOUND.equals(action)){
+                    BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    listAdapter.add(device.getName()+"\n"+device.getAddress());
+                }
+            }
+        };
+
+        registerReceiver(reciever, filter);
     }
 
     @Override
